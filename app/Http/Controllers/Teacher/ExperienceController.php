f95b8fb5073b84ed1e3b6d46c68a1dec3947aa85
hinson0 <hinson0@qq.com>
2016-11-29 15:21:00 +0800

    更新

--- app/Http/Controllers/Teacher/ExperienceController.php
+++ app/Http/Controllers/Teacher/ExperienceController.php
@@ -2,17 +2,41 @@
 
 namespace App\Http\Controllers\Teacher;
 
+use App\Models\Teacher;
 use App\Models\TeacherExperience;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class ExperienceController extends TeacherController
+class ExperienceController extends Controller
 {
-    public function show()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return view('teacher.exp.show');
+        $exps = TeacherExperience::where('teacher_id', $this->teacher_id)->simplePaginate(10);
+        return view('teacher.exp.index', ['exps' => $exps]);
     }
 
-    public function save(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('teacher.exp.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
         $this->validate($request, [
             'title' => 'required|between:2,20',
@@ -37,6 +61,77 @@ class ExperienceController extends TeacherController
         $exp->description = $request->input('description');
         $exp->save();
 
-        return response()->json(['msg' => '保存成功']);
+        return response()->json(['msg' => '保存成功', 'exp' => $exp]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $exp = TeacherExperience::find($id);
+        return response()->json($exp);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title' => 'required|between:2,20',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date',
+            'description' => 'required|between:20,255',
+        ]);
+
+        if (strtotime($request->input('end_time')) < strtotime($request->input('start_time'))) {
+            return response()->json(['msg' => '起始时间应小于结束时间'], 400);
+        }
+        $exp = TeacherExperience::find($id);
+        if (empty($exp)) {
+            return response()->json(['msg' => '非法ID'], 400);
+        }
+        $exp->title = $request->input('title');
+        $exp->start_time = strtotime($request->input('start_time'));
+        $exp->end_time = strtotime($request->input('end_time'));
+        $exp->description = $request->input('description');
+        $exp->save();
+
+        return response()->json(['msg' => '保存成功', 'exp' => $exp]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $exp = TeacherExperience::find($id);
+        if (empty($exp)) {
+            return response()->json(['msg' => '非法ID'], 400);
+        }
+        $exp->delete();
+        return response()->json(['msg' => '删除成功']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,8 @@ Route::group(['domain' => env('TEACHER_DOMAIN', 'laoshi.jiaoyu.com')], function 
         Route::get('/personal', 'Teacher\HomeController@personal');
         Route::get('/info/show', 'Teacher\InformationController@show');
         Route::post('/info/save', 'Teacher\InformationController@save');
-        Route::get('/exp/show', 'Teacher\ExperienceController@show');
-        Route::post('/exp/save', 'Teacher\ExperienceController@save');
-//        Route::get('/feature/show', 'Teacher\FeatureController@show');
-//        Route::post('/feature/save', 'Teacher\FeatureController@save');
 
+        Route::resource('/exps', 'Teacher\ExperienceController');
         Route::resource('/features', 'Teacher\FeatureController');
         Route::resource('/cases', 'Teacher\CaseController');
     });
